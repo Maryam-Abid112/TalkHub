acc792a822c36abc2980dfe3e961f11cf1a503ff
maryamabid112 <hey.maryam12@gmail.com>
2026-05-01 23:28:11 +0500

    Sending messages

--- Backend/socket/sockethandler.js
+++ Backend/socket/sockethandler.js
@@ -1,5 +1,10 @@
+const message = require("../models/Messages");
+
+
 // object for the online users 
 const onlineUsers = {};
+
+
 
 
 const socketHandler = (io) => {
@@ -10,14 +15,46 @@ const socketHandler = (io) => {
       onlineUsers[socket.userId] = socket.id;
 
       console.log("User connected");
- 
 
- // disconnecting the user
+      // now sending the messages
+
+      socket.on("send messages", async ({ recieverid, messages }) => {
+
+         //checking whether it is online or not 
+         const recieversoketid = onlineUsers[recieverid];
+
+         // store the message in database
+         const newmessage = await message.create({
+            sender: socket.userId,
+            recieverid,
+            text: message
+         })
+
+
+
+         // if user online then send message
+         if (recieversoketid) {
+            io.to(recieversoketid).emit("recieve message", {
+               senderid: socket.userId,
+               message
+
+            });
+
+         }
+
+      })
+
+
+      // disconnecting the user
       socket.on("disconnect", () => {
          delete onlineUsers[socket.userId];
       });
 
    });
+
+
+
+
 
 };
 
